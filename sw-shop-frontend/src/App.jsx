9d09c72494c454b57acdc9406af03d4adaee881b
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-14 14:47:15 +0200

    Initial frontend commit

---
+++ sw-shop-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route path={"/"} element={<CalendarPage/>}/>
+        //         <Route path={"events/:id"} element={<EventDetails/>}/>
+        //     </Routes>
+        // </BrowserRouter>
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default App;
